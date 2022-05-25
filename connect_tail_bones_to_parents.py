0951cc3e620277272556478f49898c3fb6c0be7f
abrasic <43157991+abrasic@users.noreply.github.com>
2022-05-25 11:02:38 -0700

    Update connect_tail_bones_to_parents.py

--- connect_tail_bones_to_parents.py
+++ connect_tail_bones_to_parents.py
@@ -1,8 +1,8 @@
-# Simple script to quickly retarget the tails of bones to the parent's bone head.
+# Simple script to quickly retarget the tails of selected bones to the parent's bone head.
 # You must be in edit mode and this will only target the first armature
 
 import bpy
 
 for bone in bpy.data.armatures[0].edit_bones:
-    if bone.parent is not None:
+    if bone.parent is not None and bone.select:
         bone.parent.tail = bone.head
