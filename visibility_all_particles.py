76c577f5a1805cd308a4d54ca88d6b05301871b2
abrasic <43157991+abrasic@users.noreply.github.com>
2023-04-22 13:57:59 -0700

    Update visibility_all_particles.py

--- visibility_all_particles.py
+++ visibility_all_particles.py
@@ -1,12 +1,9 @@
 # Quickly changes visibility of objects with several particle systems for the current active object.
 
-#  True = Show all particle systems. False = Hide all particle systems.
-visible = True
-
 import bpy
 obj = bpy.context.active_object
 
 if obj:
     for particle in obj.modifiers:
         if "ParticleSystem" in particle.name:
-            particle.show_viewport = visible
+            particle.show_viewport = not particle.show_viewport
